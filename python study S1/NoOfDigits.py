9da83ed7f14f5e1bec7d56e909894634e414b744
alenpaul2001 <alenpaul2001@gmail.com>
2020-04-19 17:03:15 +0530

    Organised Everything

--- python study S1/NoOfDigits.py
+++
@@ -1,6 +0,0 @@
-n=input("Enter")
-counter=0
-while(n>0):
-	counter=counter+1
-	n=n/10
-print counter
--- python study S1/SumOfDigits.py
+++
@@ -1,7 +0,0 @@
-n=input("enter")
-reverse=0
-while(n>0):
-	re=n%10
-	reverse=reverse+re
-	n=n/10
-print reverse
