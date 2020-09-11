ca945ea782b587357b0e134c027388b1bf71c3a3
alenpaul2001 <alenpaul2001@gmail.com>
2020-09-11 23:44:48 +0530

    enhanced by flake8

--- auto_wallpaper/auto_wallpaper.py
+++ auto_wallpaper/auto_wallpaper.py
@@ -7,25 +7,29 @@ import time
 home = os.getcwd().split('/')[:3]
 HOME = (f'/{home[1]}/{home[2]}')
 
-wallpapper_folder = os.path.join(HOME,".local/share/backgrounds/")
-temp_folder = os.path.join(HOME,".local/share/backgrounds/temp/wall.jpeg")
+wallpapper_folder = os.path.join(HOME, ".local/share/backgrounds/")
+temp_folder = os.path.join(HOME, ".local/share/backgrounds/temp/wall.jpeg")
 
 
 def copy(source):
-    shutil.copy(source,temp_folder)
+    shutil.copy(source, temp_folder)
+
+
 def mover(source):
-    shutil.move(source,wallpapper_folder)
+    shutil.move(source, wallpapper_folder)
+
+
 def delete(source):
     os.remove(source)
 
+
 while True:
     for file in os.listdir(f'{HOME}/Pictures/wallpaper'):
-        path_of_file = os.path.join(f'{HOME}/Pictures/wallpaper',file)
-        if (os.path.isfile(path_of_file)) and (path_of_file!=os.path.join(f'{HOME}/Pictures/wallpaper','auto_wallpaper.py')):
+        path_of_file = os.path.join(f'{HOME}/Pictures/wallpaper', file)
+        if (os.path.isfile(path_of_file)) and (path_of_file != os.path.join(f'{HOME}/Pictures/wallpaper', 'auto_wallpaper.py')):
             print(path_of_file)
             copy(path_of_file)
-            delete(os.path.join(wallpapper_folder,'wall.jpeg'))
+            delete(os.path.join(wallpapper_folder, 'wall.jpeg'))
             mover(temp_folder)
             # Change The Value As Your Likings
             time.sleep(100)
-        
