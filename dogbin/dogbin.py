5bab94e47824c00f46c30e966d5b6f90a9a98236
alenpaul2001 <alenpaul2001@gmail.com>
2020-09-11 23:44:42 +0530

    enhanced by flake8

--- dogbin/dogbin.py
+++ dogbin/dogbin.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-#(c) AlenPaulVarghese
+# (c) AlenPaulVarghese
 import requests
 import sys
-import os 
+import os
 
 '''
 --------------------------------------------------------------------------
@@ -11,12 +11,14 @@ cat example.txt | ./dogbin.py or cat example.txt | python3 dogbin.py
 --------------------------------------------------------------------------
 To get short links -->
 python3 dogbin.py https://example.com or ./dogbin.py https://example.com
--------------------------------------------------------------------------- 
+--------------------------------------------------------------------------
 For Multiline -->
 python3 dogbin.py and paste the content in nano editor
-and save the file without renaming 
+and save the file without renaming
 --------------------------------------------------------------------------
 '''
+
+
 def deldog(message):
     url = "https://del.dog/documents"
     r = requests.post(url, data=message.encode("UTF-8")).json()
@@ -26,17 +28,16 @@ def deldog(message):
 message = ''
 if not sys.stdin.isatty():
     for lines in sys.stdin:
-        message+=lines
+        message += lines
     deldog(message)
 else:
     try:
         deldog(list(sys.argv)[1])
     except IndexError:
         os.system("nano PasteYouContentHere-Dont-Change-File-Name")
-        with open("PasteYouContentHere-Dont-Change-File-Name","rb") as nano:
+        with open("PasteYouContentHere-Dont-Change-File-Name", "rb") as nano:
             lines = nano.readlines()
             for texts in lines:
-                message+=texts.decode('UTF-8') 
+                message += texts.decode('UTF-8')
         os.remove("PasteYouContentHere-Dont-Change-File-Name")
         deldog(message)
-
